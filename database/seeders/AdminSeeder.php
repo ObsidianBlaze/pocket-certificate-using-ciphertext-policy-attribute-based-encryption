52afcfb206c9591a6cee6fba22505a85c8410c0a
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2023-01-09 17:32:29 +0100

    Update AdminSeeder.php

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('admins')->insert([
+            'email' => "admin@mail.com",
+            'role_id'=> 1,
+            'password' => Hash::make('admin123'),
+        ]);
     }
 }

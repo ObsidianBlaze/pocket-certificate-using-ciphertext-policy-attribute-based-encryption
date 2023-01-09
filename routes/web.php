8030862463e818e03b8c39b9ec0dce4b9b0c349f
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2023-01-10 00:53:03 +0100

    feat: working on the login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/student_login', [LoginController::class, 'loginView']);
+
+

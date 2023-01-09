da57173427a6826b33c5abe74f811ef28d5cd22d
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2023-01-09 17:24:58 +0100

    Update 2023_01_09_154759_create_admins_table.php

--- database/migrations/2023_01_09_154759_create_admins_table.php
+++ database/migrations/2023_01_09_154759_create_admins_table.php
@@ -16,6 +16,7 @@ class CreateAdminsTable extends Migration
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
+            $table->string('password');
             $table->unsignedBigInteger('role_id')->index();
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->timestamps();

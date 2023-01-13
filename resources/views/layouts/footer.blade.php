537a48abcf42c404693698be03d94ee892776f2f
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2023-01-14 00:41:17 +0100

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,21 @@
+
+<footer style="background-color: gray" class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <span style="color: black;" class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 Laspotech™. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0" style="color: black;">
+        <li>
+            <a href="{{url('/about')}}" class="mr-4 hover:underline md:mr-6 ">About</a>
+        </li>
+        <li>
+            <a href="{{url('/#news')}}" class="mr-4 hover:underline md:mr-6">News</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline">Contact</a>
+        </li>
+    </ul>
+</footer>
+
+
 <script>
     function toggleMenu (btn) {
         const el = btn.parentElement.querySelector('.subMenu')

7f045d6f652fa34281ab766bfc86510f7e9b0c00
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2023-01-14 01:43:14 +0100

    Update studentlogin.blade.php

--- resources/views/student/studentlogin.blade.php
+++ resources/views/student/studentlogin.blade.php
@@ -1,4 +1,12 @@
-@include('layouts.main')
+<!doctype html>
+<html lang="en">
+@include('layouts.navstyle')
+<body>
+<section class="mb-40">
+
+    @include('layouts.nav')
+
+</section>
 @if(session('successMsg'))
     <div class="flex p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-gray-800 dark:text-green-400" role="alert">
         <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
@@ -26,7 +34,7 @@
 @endif
 
 <section class="bg-gray-50 dark:bg-gray-900">
-    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto sm:h-screen lg:py-0">
         <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
             <img class="w-18 h-18 mr-2" src="{{asset('img/newlogo.jpg')}}" alt="logo">
         </a>
@@ -55,3 +63,8 @@
         </div>
     </div>
 </section>
+
+</body>
+@include('layouts.footer')
+</html>
+

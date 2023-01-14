d94b372cee91312ed9736f1018f64309fc1696a7
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2023-01-14 01:51:34 +0100

    Update nav.blade.php

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -46,7 +46,7 @@
                     </button>
                     <ul class="text-sm subMenu hidden border-gray-100 rounded-lg bg-gray-50 md:flex-row md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <br>
-                        <a href="#"
+                        <a href="{{url('student/login')}}"
                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Student</a>
                         <br>
 

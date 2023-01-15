8e4b384b07afeb65748cd26a9d21daecc4f92014
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2023-01-15 19:10:56 +0100

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,9 +1,7 @@
 
 @include('admin.dashboardtop')
     <!-- content -->
-    <div class="flex-1 p-10 text-2xl font-bold">
-        <section class="bg-gray-50 dark:bg-gray-900">
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <div class="flex-1 p-10 text-2xl font-bold" style="margin-left: 20%;">
 
                 <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -42,7 +40,5 @@
                     </div>
                 </div>
 
-            </div>
-        </section>
     </div>
 @include('admin.dashboardbottom')

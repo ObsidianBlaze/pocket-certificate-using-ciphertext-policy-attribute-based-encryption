366ff8a6cc30da56856031bd88c9bec0be4be242
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2023-01-19 08:24:27 +0100

    Update upload_certificate.blade.php

--- resources/views/admin/upload_certificate.blade.php
+++ resources/views/admin/upload_certificate.blade.php
@@ -2,10 +2,10 @@
 <!-- content -->
 
 <div class="flex-1 p-10 text-2xl font-bold" style="margin-left: 17%;">
-    <form class="w-full max-w-sm">
+    <form style="font-size: large;">
         <div class="flex items-center border-b border-teal-500 py-2">
-            <label for="level">Level:</label>
-            <select name="level" id="level" value="{{old('level') }}">
+            <label for="level" style="padding-right: 10px;" >Level:</label>
+            <select name="level" id="level" value="{{old('level') }}" style="width: 120px;">
                 <option value="" selected="selected">Select Level...</option>
                 @foreach($levels as $level)
                     <option value="{{$level -> id}}">{{$level -> name}}</option>
@@ -16,11 +16,11 @@
                 <input type="number" hidden disabled value="{{$admin -> id}}">
             @endforeach
 
-            <label for="mat_number" style="padding-left: 20px;">Mat Number:</label>
+            <label for="mat_number" style="padding-right: 10px;">Mat Number:</label>
             <input type="number" name="mat_number" style="border: 1px solid black">
 
             <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Sign in
+                GO!
             </button>
         </div>
     </form>

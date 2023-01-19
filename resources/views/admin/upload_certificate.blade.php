f8086b7da4c0659a95e10727119c488573fbb01a
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2023-01-19 08:53:52 +0100

    Update upload_certificate.blade.php

--- resources/views/admin/upload_certificate.blade.php
+++ resources/views/admin/upload_certificate.blade.php
@@ -2,17 +2,25 @@
 <!-- content -->
 
 <div class="flex-1 p-10 text-2xl font-bold" style="margin-left: 17%;">
-    <form style="font-size: large;">
+    <form style="font-size: medium;">
         <div class="flex items-center border-b border-teal-500 py-2">
             <label for="level" style="padding-right: 10px;" >Level:</label>
             <select name="level" id="level" value="{{old('level') }}" style="width: 120px;">
-                <option value="" selected="selected">Select Level...</option>
+                <option value="" selected="selected">Select...</option>
                 @foreach($levels as $level)
                     <option value="{{$level -> id}}">{{$level -> name}}</option>
                 @endforeach
             </select>
 
-            @foreach($user as $admin)
+            <label for="department" style="padding-right: 10px;" >Department:</label>
+            <select name="department" id="level" value="{{old('department') }}" style="width: 120px;">
+                <option value="" selected="selected">Select...</option>
+                @foreach($departments as $department)
+                    <option value="{{$department -> id}}">{{$department -> name}}</option>
+                @endforeach
+            </select>
+
+        @foreach($user as $admin)
                 <input type="number" hidden disabled value="{{$admin -> id}}">
             @endforeach
 
@@ -24,6 +32,60 @@
             </button>
         </div>
     </form>
+
+
+
+    <div class="relative overflow-x-auto" style="margin-top: 70px;">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-9 py-3">
+                    Matric_No
+                </th>
+                <th scope="col" class="px-9 py-3">
+                    Department
+                </th>
+                <th scope="col" class="px-9 py-3">
+                    Level
+                </th>
+                <th scope="col" class="px-9 py-3">
+                    Admin_Email
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-9 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    2005022029
+                </th>
+                <td class="px-9 py-4">
+                    Computer Science
+                </td>
+                <td class="px-9 py-4">
+                    ND
+                </td>
+                <td class="px-9 py-4">
+                    admin@mail.com
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-9 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    2005022032
+                </th>
+                <td class="px-9 py-4">
+                    Computer Science
+                </td>
+                <td class="px-9 py-4">
+                    HND
+                </td>
+                <td class="px-9 py-4">
+                    admin@mail.com
+                </td>
+            </tr>
+
+            </tbody>
+        </table>
+    </div>
 
 
 </div>
